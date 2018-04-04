5b165b7da13658233e296f092e769fae40b27e22
Sebastian Axelsen <sa@palmtreestatistics.com>
2018-04-04 11:32:05 +0200

    Adds random_quote generator

--- reply_handler.py
+++ reply_handler.py
@@ -1,24 +1,40 @@
 import random
 import re
 from replies import eightball_replies
+import json
+import time
 
 random.seed(1234)
 
 QUESTION_REGEX = re.compile(r'.+\?')
 
+with open('random_quotes.json', 'r') as random_quotes_file:
+    random_quotes = json.load(random_quotes_file)
+    random_quotes = [quote['quote'] for quote in random_quotes]
+
 
 def get_reply(message):
     message_text = message.text
 
+    # Add delay to simulate bot typing
+    time.sleep(0.5)
+
     if QUESTION_REGEX.match(message_text):
         return eightball_reply(message)
     else:
-        return 'I\'m not sure what to say.'
+        return random_quote(message)
     pass
 
 
 def eightball_reply(message):
     random_index = random.randint(0, len(eightball_replies) - 1)
     reply = eightball_replies[random_index]
+
+    return reply
+
+
+def random_quote(message):
+    random_index = random.randint(0, len(random_quotes) - 1)
+    reply = random_quotes[random_index]
 
     return reply
